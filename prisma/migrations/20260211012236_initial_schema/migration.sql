f208eb3db810b16209403a25accc399ed7d1283b
Walter Furrer <walterfurrer.dev@gmail.com>
2026-02-11 19:41:16 -0500

    Walter/fb 24 design and implement database schema (#2)
    * Started setting up schema in schema.prisma
    
    * we have a database

---
+++ prisma/migrations/20260211012236_initial_schema/migration.sql
@@ -0,0 +1,123 @@
+-- CreateEnum
+CREATE TYPE "AnimeStatus" AS ENUM ('PLAN_TO_WATCH', 'WATCHING', 'COMPLETED', 'DROPPED', 'ON_HOLD');
+
+-- CreateEnum
+CREATE TYPE "MangaStatus" AS ENUM ('PLAN_TO_READ', 'READING', 'COMPLETED', 'DROPPED', 'ON_HOLD');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "ipAddress" TEXT,
+    "userAgent" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "accessToken" TEXT,
+    "refreshToken" TEXT,
+    "accessTokenExpiresAt" TIMESTAMP(3),
+    "refreshTokenExpiresAt" TIMESTAMP(3),
+    "scope" TEXT,
+    "idToken" TEXT,
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Verification" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Verification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AnimeListEntry" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "malId" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "japaneseTitle" TEXT NOT NULL,
+    "englishTitle" TEXT NOT NULL,
+    "status" "AnimeStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "AnimeListEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MangaListEntry" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "malId" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "japaneseTitle" TEXT NOT NULL,
+    "englishTitle" TEXT NOT NULL,
+    "status" "MangaStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "MangaListEntry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_token_key" ON "Session"("token");
+
+-- CreateIndex
+CREATE INDEX "AnimeListEntry_userId_idx" ON "AnimeListEntry"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AnimeListEntry_malId_userId_key" ON "AnimeListEntry"("malId", "userId");
+
+-- CreateIndex
+CREATE INDEX "MangaListEntry_userId_idx" ON "MangaListEntry"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MangaListEntry_malId_userId_key" ON "MangaListEntry"("malId", "userId");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AnimeListEntry" ADD CONSTRAINT "AnimeListEntry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MangaListEntry" ADD CONSTRAINT "MangaListEntry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
